bf0c0afaffdc89c16b0a69c182729f4a83c05133
davidkneely <contactvisionrunner@gmail.com>
2025-03-30 22:55:11 -1000

    Adds posts.json and BlogPosts component to pull from this json file

--- src/components/Blog.js
+++ src/components/Blog.js
@@ -1,26 +1,12 @@
 import React from 'react';
 import '../App.css';
+import BlogPosts from './BlogPosts';
 
 function Blog() {
   return (
     <div className="content">
       <div className="blog-section">
-        <h1 className="section-title">Blog Posts</h1>
-        <div className="blog-posts">
-          {/* Example blog post */}
-          <article className="blog-post">
-            <h2>Latest Blog Post Title</h2>
-            <div className="post-meta">
-              <span className="post-date">January 1, 2024</span>
-              <span className="post-category">Technology</span>
-            </div>
-            <p className="post-excerpt">
-              This is a preview of the blog post content. Click to read more about this interesting topic...
-            </p>
-            <a href="#" className="read-more">Read More</a>
-          </article>
-          {/* Add more blog posts as needed */}
-        </div>
+        <BlogPosts />
       </div>
     </div>
   );
---
+++ src/components/BlogPosts.js
@@ -0,0 +1,41 @@
+import { useEffect, useState } from "react";
+
+const BlogPosts = () => {
+  const [posts, setPosts] = useState([]);
+
+  useEffect(() => {
+    fetch("/posts.json")
+      .then((response) => response.json())
+      .then((data) => setPosts(data.posts))
+      .catch((error) => console.error("Error fetching blog posts:", error));
+  }, []);
+
+  return (
+    <div className="blog-content">
+      <h1 className="section-title">Blog Posts</h1>
+      <div className="blog-posts">
+        {posts.map((post) => (
+          <article key={post.id} className="blog-post">
+            <h2>{post.title}</h2>
+            <div className="post-meta">
+              <span className="post-date">{new Date(post.date).toLocaleDateString('en-US', {
+                year: 'numeric',
+                month: 'long',
+                day: 'numeric'
+              })}</span>
+              <div className="post-tags">
+                {post.tags.map((tag, index) => (
+                  <span key={index} className="post-tag">{tag}</span>
+                ))}
+              </div>
+            </div>
+            <div className="post-content" dangerouslySetInnerHTML={{ __html: post.htmlContent }} />
+            <a href="#" className="read-more">Read More</a>
+          </article>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default BlogPosts;
